7aa0d0c0ea7534cc5d2e8b023e6d1b80d7de9e17
Alex Balak <alex.balak@outlook.com>
2025-10-15 11:42:13 +0200

    switched to JDBC Template Database connection

--- src/main/java/app/controller/ProductController.java
+++ src/main/java/app/controller/ProductController.java
@@ -2,7 +2,6 @@ package app.controller;
 
 import app.model.Product;
 import app.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,6 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
