fa0367aa65448f62d9dff9d9e7fff21cc7d60352
Alex Balak <alex.balak@outlook.com>
2025-10-21 17:13:11 +0200

    created /login method, need to implement it

--- src/main/java/app/controller/AuthController.java
+++ src/main/java/app/controller/AuthController.java
@@ -1,5 +1,18 @@
 package app.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
 public class AuthController {
+    
+
+    @PostMapping("/login")
+    public String login(@RequestBody String request) {
+        //TODO: process POST request
+        return request;
+    }
     
 }
